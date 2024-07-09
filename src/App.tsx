48f7948d31d818cd217c084830692345037b8411
Berenice Barcenas Mancilla <berenice.barcenas.mancilla@gmail.com>
2024-07-08 19:40:18 -0600

    correcciones de sintaxis

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ import {
   AllUsers,
 } from "@/_root/pages";
 import AuthLayout from "./_auth/AuthLayout";
-import RootLayout from "./_root/RootLayout";
-import SignupForm from "@/_auth/forms/SignupForm";
-import SigninForm from "@/_auth/forms/SigninForm";
+import RootLayout from "./_root/RootLayot";
+import SigninForm from "./_auth/forms/SinginForm";
+import SignupForm from "./_auth/forms/SingupForm";
 import { Toaster } from "@/components/ui/toaster";
 
 import "./globals.css";
@@ -25,25 +25,25 @@ const App = () => {
       <Routes>
         {/* public routes */}
         <Route element={<AuthLayout />}>
-          <Route path="/sign-in" element={<SinginForm/>} />
-          <Route path="/sign-up" element={<SignupForm/>} />
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
         </Route>
 
         {/* private routes */}
-        <Route element={<RootLayout/>}>
+        <Route element={<RootLayout />}>
           <Route index element={<Home />} />
-          { <Route path="/explore" element={<Explore />} />
+          <Route path="/explore" element={<Explore />} />
           <Route path="/saved" element={<Saved />} />
           <Route path="/all-users" element={<AllUsers />} />
           <Route path="/create-post" element={<CreatePost />} />
           <Route path="/update-post/:id" element={<EditPost />} />
           <Route path="/posts/:id" element={<PostDetails />} />
           <Route path="/profile/:id/*" element={<Profile />} />
-          <Route path="/update-profile/:id" element={<UpdateProfile />} /> }
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
 
-       <Toaster /> 
+      <Toaster />
     </main>
   );
 };
